7b52ab47c6f0d95dfb1ac901e2e50085eaa6ae91
abdul23lm <abdul23lm@gmail.com>
2020-08-23 09:50:40 +0700

    Add firebase defedencies

--- lib/main.dart
+++ lib/main.dart
@@ -98,7 +98,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Simple counter count:',
             ),
             Text(
               '$_counter',
